07d3daf8986328254b822983c0442c45b51a11ab
izmashafqat07 <izmashafqat148@gmail.com>
2025-09-17 17:46:51 +0500

    added taxonomy level in clo model and view

--- addons/obesystem/models/clo.py
+++ addons/obesystem/models/clo.py
@@ -8,3 +8,19 @@ class CLO(models.Model):
     description = fields.Text('CLO Description')
     plo_ids = fields.Many2many('obesystem.plo', string='Associated PLOs')
     course_id = fields.Many2one('obesystem.course', string='Course')
+
+    # New Taxonomy Level field
+    TAXONOMY_SELECTION = [
+        ('knowledge', 'Knowledge'),
+        ('comprehension', 'Comprehension'),
+        ('application', 'Application'),
+        ('analysis', 'Analysis'),
+        ('synthesis', 'Synthesis'),
+        ('evaluation', 'Evaluation'),
+    ]
+    taxonomy_level = fields.Selection(
+        TAXONOMY_SELECTION,
+        string='Taxonomy Level',
+        required=True,
+        default='knowledge'
+    )
