5de790707a773fd90011d6eba46c564f29e4ee1a
izmashafqat07 <izmashafqat148@gmail.com>
2025-09-17 15:12:39 +0500

    removed fbr modules

--- addons/fbr_fbr_integration_errror/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import res_company
-from . import res_config_settings
-from . import uom_fbr
-from . import product_template
-from . import product_template_measures
-from . import account_move
