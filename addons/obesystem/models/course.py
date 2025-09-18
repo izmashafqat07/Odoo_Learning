355480f6e55a2ab1cbe261349f86074f31ce5db2
izmashafqat07 <izmashafqat148@gmail.com>
2025-09-18 13:46:03 +0500

    updated clo view too

--- addons/obesystem/models/course.py
+++ addons/obesystem/models/course.py
@@ -7,9 +7,12 @@ class Course(models.Model):
     
     code = fields.Char('Course Code', required=True)
     title = fields.Char('Course Title', required=True)
-    prereq_course_id = fields.Many2one('obesystem.course', string='Prerequisite Course')
-    coreq_course_id = fields.Many2one('obesystem.course', string='Co-requisite Course')
+    # Changed from Many2one to Many2many
+    prereq_course_ids = fields.Many2many('obesystem.course', 'course_prerequisite_rel', 'course_id', 'prereq_id', string='Prerequisite Courses')
+    coreq_course_ids = fields.Many2many('obesystem.course', 'course_corequisite_rel', 'course_id', 'coreq_id', string='Co-requisite Courses')
     description = fields.Text('Course Description')
+    course_content = fields.Text('Course Content', 
+        help="Enter course topics as comma-separated values (e.g., Introduction to Programming, Variables, Data Types)")
     clo_ids = fields.Many2many('obesystem.clo', string='Associated CLOs')
     textbook_ids = fields.One2many('obesystem.textbook', 'course_id', string='Textbooks')
     
