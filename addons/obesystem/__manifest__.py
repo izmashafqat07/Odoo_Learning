f45b7f1914a76ce16dfe427f0ea7e5f8bd1dd2a4
izmashafqat07 <izmashafqat148@gmail.com>
2025-09-19 13:33:50 +0500

    made some changes in manifest file

--- addons/obesystem/__manifest__.py
+++ addons/obesystem/__manifest__.py
@@ -2,16 +2,22 @@
     'name': 'OBE University Management System',
     'version': '1.0',
     'category': 'Education',
-    'author': 'Your Name',
+    'author': 'Izma Shafqat',
     'website': 'https://yourwebsite.com',
-    'depends': ['base'],
+    'depends': ['base', 'web'],
     'data': [
         'security/ir.model.access.csv',
         'views/peo_view.xml',
         'views/plo_view.xml',
         'views/clo_view.xml',
         'views/course_view.xml',
+       
     ],
     'installable': True,
     'application': True,
+    # 'assets': {
+    #     'web.assets_backend': [
+    #         'obesystem/static/src/scss/custom_colors.scss',
+    #     ],
+    # },
 }
