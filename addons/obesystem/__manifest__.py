a29948260fc1d2c44f6199a9c69d32b2986f26a9
izmashafqat07 <izmashafqat148@gmail.com>
2025-09-22 10:35:51 +0500

    minor theme changes are done

--- addons/obesystem/__manifest__.py
+++ addons/obesystem/__manifest__.py
@@ -3,7 +3,6 @@
     'version': '1.0',
     'category': 'Education',
     'author': 'Izma Shafqat',
-    # 'website': 'https://yourwebsite.com',
     'depends': ['base', 'web'],
     'data': [
         'security/ir.model.access.csv',
@@ -11,13 +10,13 @@
         'views/plo_view.xml',
         'views/clo_view.xml',
         'views/course_view.xml',
-       
     ],
+    'assets': {
+        'web.assets_backend': [
+            'obesystem/static/src/scss/custom_theme.scss',
+            'obesystem/static/src/js/custom_buttons.js',
+        ],
+    },
     'installable': True,
     'application': True,
-    # 'assets': {
-    #     'web.assets_backend': [
-    #         'obesystem/static/src/scss/custom_colors.scss',
-    #     ],
-    # },
 }
